baa14deaf203e14ebb06dc722000be538381d097
Ismail Jalloh <ijalloh21@gmail.com>
2019-01-04 19:31:29 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -7,5 +7,10 @@ def my_all?(collection)
     block_return_values << yield(collection[i])
     i += 1
   end
-  include?
+  
+  if block_return_values.include?(false)
+    false
+  else
+    true
+  end
 end
